345b841a7db9c0fd22b2913bec6513b24830f04c
Justin <jpicar@berkeley.edu>
2017-12-13 18:21:28 -0800

    final commit

--- app.py
+++ app.py
@@ -21,73 +21,74 @@ dynamodb_client = boto3.client("dynamodb", region_name="us-west-2",
                                endpoint_url="https://dynamodb.us-"
                                + "west-2.amazonaws.com")
 
+"""AWS DynamoDB checks to see if a table "Reasons" exists.
+If not, it creates a new table called "Reasons", and
+populates it with information from Reasons.csv.
 
-def dynamodb_create_table():
-    """AWS DynamoDB creates a new table called "Reasons".
-    The table is populated with information from Reasons.csv.
-
-    """
-    if reasons not in dynamodb_client.list_tables()["TableNames"]:
-        dynamodb_client.create_table(
-            TableName=reasons,
-            KeySchema=[
-                {
-                    "AttributeName": "ID",
-                    "KeyType": "HASH"
-                },
-                {
-                    "AttributeName": "Reasons",
-                    "KeyType": "RANGE"
-                }
-            ],
-            AttributeDefinitions=[
-                {
-                    "AttributeName": "ID",
-                    "AttributeType": "N"
-                },
-                {
-                    "AttributeName": "Reasons",
-                    "AttributeType": "S"
-                }
-            ],
-            ProvisionedThroughput={
-                "ReadCapacityUnits": 10,
-                "WriteCapacityUnits": 10
+"""
+if reasons not in dynamodb_client.list_tables()["TableNames"]:
+    dynamodb_client.create_table(
+        TableName=reasons,
+        KeySchema=[
+            {
+                "AttributeName": "ID",
+                "KeyType": "HASH"
+            },
+            {
+                "AttributeName": "Reasons",
+                "KeyType": "RANGE"
             }
-        )
+        ],
+        AttributeDefinitions=[
+            {
+                "AttributeName": "ID",
+                "AttributeType": "N"
+            },
+            {
+                "AttributeName": "Reasons",
+                "AttributeType": "S"
+            }
+        ],
+        ProvisionedThroughput={
+            "ReadCapacityUnits": 10,
+            "WriteCapacityUnits": 10
+        }
+    )
 
-        # This holds the code from running, giving
-        # time for the server to create the table.
-        time.sleep(15)
+    # This holds the code from running, giving
+    # time for the server to create the table.
+    time.sleep(15)
 
-        reasons_table = dynamodb_resource.Table(reasons)
+    reasons_table = dynamodb_resource.Table(reasons)
 
-        with open("Reasons.csv", "r") as csvfile:
-            reasons_file = csv.reader(csvfile, delimiter=" ")
-            counter = 0
-            for reason in reasons_file:
-                table.put_item(
-                    Item={
-                        "ID": counter,
-                        "Reasons": " ".join(reason)
-                    }
-                )
+    with open("Reasons.csv", "r") as csvfile:
+        reasons_file = csv.reader(csvfile, delimiter=" ")
+        counter = 0
+        for reason in reasons_file:
+            reasons_table.put_item(
+                Item={
+                    "ID": counter,
+                    "Reasons": " ".join(reason)
+                }
+            )
 
-                counter += 1
+            counter += 1
 
 
-@app.schedule(Rate(1, unit=Rate.HOURS))
+@app.schedule('rate(1 hour)')
 def email_lambda_function(event):
     """AWS Lambda function that pulls from the "Reasons" DynamoDB table
     and uses SES to send an email.
 
     The decorator is an indicator for CloudWatch to create a rule and
     schedule an event for this Lambda function to run every hour.
+
     """
     try:
         reasons_table = dynamodb_resource.Table(reasons)
         rand_int = random.randint(0, 25)
-        response = table.query(KeyConditionExpression=Key("ID").eq(rand_int))
+        response = reasons_table.query(
+            KeyConditionExpression=Key("ID").eq(rand_int))
         client.send_email(
             Destination={
                 "ToAddresses": [os.environ['RECEIVER_1']]
